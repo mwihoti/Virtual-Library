ceed1886facb24cd0d4a773f397e724b97bfa701
mwihoti <danielmwihoti@gmail.com>
2024-02-02 02:35:05 +0300

    creating BookControllers and apiroute

--- virtual-backend/app/Http/Controllers/BooksController.php
+++ virtual-backend/app/Http/Controllers/BooksController.php
@@ -7,4 +7,23 @@ use Illuminate\Http\Request;
 class BooksController extends Controller
 {
     //
+    function getIndex()
+    {
+        return Book::all();
+
+    }
+    function show_id($id)
+    {
+        return Book::with('author')->find($id);
+    }
+    function book_store(Request $req)
+    {
+        return Book::create($req->all());
+    }
+    function update_books(Request $req, $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->update($req->all());
+        return $book;
+    }
 }
--- virtual-backend/routes/api.php
+++ virtual-backend/routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/books', [BooksController::class, ('getIndex')]);
+Route::get('')
