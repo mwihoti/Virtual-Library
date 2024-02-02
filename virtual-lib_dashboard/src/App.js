2f4a25a4c9d28de707d625692592568342d29191
mwihoti <danielmwihoti@gmail.com>
2024-02-02 08:36:05 +0300

    Virtual-lib dashboard components

--- virtual-lib_dashboard/src/App.js
+++ virtual-lib_dashboard/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import AuthorList from './AuthorList';
+import BookList from './BookList';
 
 function App() {
   return (
     <div >
       
-     
+     <h1>virtual Library</h1>
+     <BookList />
+     <AuthorList />
     </div>
   );
 }
